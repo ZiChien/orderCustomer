8112269dc3fb580954cff995bd72eb7b2ed6a87a
ZiChien <tanyeee226@gmail.com>
2024-08-16 16:24:51 +0800

    Fix:serializableCheck

--- src/store.js
+++ src/store.js
@@ -2,9 +2,18 @@ import { configureStore } from '@reduxjs/toolkit'
 import cartReducer from './store/cartSlice'
 import merchantReducer from './store/merchantSlice'
 import orderSlice from './store/orderSlice'
-import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // 默認使用 LocalStorage
 import { combineReducers } from 'redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 
 const rootReducer = combineReducers({
   cart: cartReducer,
@@ -21,7 +30,13 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 const store = configureStore({
   reducer: persistedReducer,
-});
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
 
 // 創建 persistor 用於持久化操作
 const persistor = persistStore(store);
